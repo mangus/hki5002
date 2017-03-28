58e5fdef618c31c9c039d23bed107cdf2a6e6e9b
Mart Mangus <mart@mangus.ee>
2017-03-28 19:21:28 +0300

    Esimese sessi materjalid

---
+++ sess1/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8; width=device-width, initial-scale=1.0" name="viewport">
+    <title>HKI5002: 1. sessioon</title>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/styles/materialize.min.css">
+    <link rel="stylesheet" href="../assets/styles/main.css">
+</head>
+<body>
+    <div class="page-wrap container">
+        <h1 class="main-heading">HKI5002 esimene sessioon</h1>
+        <div class="divider"></div>
+        <div class="section">
+            <h3>Mis täna plaanis?</h3>
+            <ul>
+                <li>Saame tuttavaks</li>
+                <li>Ülevaade aine korraldusest</li>
+                <li>Kiire kordamine</li>
+                <li>Lihtsamate ülesannetega alustamine</li>
+            </ul>
+        </div>
+
+        <div class="section">
+            <h3>Õppe korraldus</h3>
+            <ul>
+                <li>5 kontakt sessiooni: lahendame koos ülesandeid</li>
+                <li>Iga nädal töö + aruanne (40% hindest)</li>
+                <li>Kodune projekt</li>
+                <li>Eksam = koduse projekti kaitsmine (60% hindest)</li>
+            </ul>
+        </div>
+
+        <div class="section">
+            <h3>Kordamine</h3>
+            <ul>
+                <li>Versioonihaldus: commit, push, pull</li>
+                <li>Java, Javascript, klient, server</li>
+                <li>Muutuja, massiiv, meetod?</li>
+                <li>Klass, objekt?</li>
+                <li>Staatiline meetod?</li>
+                <li>Abstraktne klass?</li>
+                <li>SSH, SSL?</li>
+            </ul>
+        </div>
+
+        <div class="section">
+            <h3>Ülesanded</h3>
+            Kasutame <a href="http://minitorn.tlu.ee/~jaagup/kool/java/loeng/veebijatk/veebijatk.pdf">Jaagup Kippari materjale</a> ja võtame ette:
+            <ol>
+                <li>kalkulaatori ülesande leheküljelt 6,</li>
+                <li>seejärel pane käima graafika näide "Ristkülik ekraanil" (leheküljel 7),</li>
+                <li>foor (lehekülg 8).</li>
+                <li>Tõstame kirjutatud koodi versioonihaldusesse.</li>
+                <li>Jätkame ülesannetega leheküljelt 10.</li>
+                <li>Enne praktikumide lõppu veendu, et kood oleks versioonihalduses!</li>
+            </ol>
+        </div>
+    </div>
+</body>
+</html>
+
