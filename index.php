156797e65d4654af298888815c65b5cb4a6d8faa
martti <mrt@mrt.ee>
2017-04-11 22:29:04 +0300

    Hinnetelehe link avalehele

--- index.php
+++ index.php
@@ -1,184 +1,189 @@
 <!DOCTYPE html>
 <html lang="et">
 <head>
-	<meta charset="utf-8">
-	<title>HKI5002</title>
-	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link rel="stylesheet" href="assets/styles/materialize.min.css">
-	<link rel="stylesheet" href="assets/styles/main.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="utf-8">
+    <title>HKI5002</title>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="assets/styles/materialize.min.css">
+    <link rel="stylesheet" href="assets/styles/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
-	<a href="https://github.com/mangus/hki5002" class="github-corner" aria-label="View source on Github">
-		<svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true">
-			<path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
-			<path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
-			<path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
-		</svg>
-	</a>
-	<div class="page-wrap container">
-	<h1 class="main-heading">Programmeerimine I
-		<small>HKI5002</small>
-	</h1>
-	<div class="divider"></div>
+<a href="https://github.com/mangus/hki5002" class="github-corner" aria-label="View source on Github">
+    <svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true">
+        <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
+        <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
+        <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
+    </svg>
+</a>
+<div class="page-wrap container">
+    <h1 class="main-heading">Programmeerimine I
+        <small>HKI5002</small>
+    </h1>
+    <div class="divider"></div>
 
-	<div class="section">
-		<div class="row">
-			<div class="col s6">
-				<h3>Auditoorne õppetöö</h3>
-				<ul>
-					<li><a href="sess1/">1. sessioon</a> (30. märts 2017)</li>
-					<li>2. sessioon (20. aprill 2017)</li>
-					<li>3. sessioon (4. mai 2017)</li>
-					<li>4. sessioon (18. mai 2017)</li>
-					<li>5. sessioon (1. juuni 2017)</li>
-					<li>Eksam (15. juuni 2017)</li>
-				</ul>
-			</span>
-			</div>
-			<div class="col s6">
-				<h3>Õppejõud</h3>
-				<p>Mart Mangus / <a href="mailto:mart@mangus.ee">mart@mangus.ee</a></p>
-			</div>
-		</div>
-	</div>
+    <div class="section">
+        <div class="row">
+            <div class="col s6">
+                <h3>Auditoorne õppetöö</h3>
+                <ul>
+                    <li><a href="sess1/">1. sessioon</a> (30. märts 2017)</li>
+                    <li>2. sessioon (20. aprill 2017)</li>
+                    <li>3. sessioon (4. mai 2017)</li>
+                    <li>4. sessioon (18. mai 2017)</li>
+                    <li>5. sessioon (1. juuni 2017)</li>
+                    <li>Eksam (15. juuni 2017)</li>
+                </ul>
+                </span>
+            </div>
+            <div class="col s6">
+                <h3>Õppejõud</h3>
+                <p>Mart Mangus / <a href="mailto:mart@mangus.ee">mart@mangus.ee</a></p>
+            </div>
+        </div>
+    </div>
 
-	<div class="divider"></div>
+    <div class="divider"></div>
 
-	<div class="section">
-		<h3>Iseseisev õppetöö</h3>
-		<p>Iga nädal tuleb iseseisvalt lahendada ette antud ülesanded ja/või arendada oma kodust projekti.<br />
-		Tulemused on vaja saata <a target="_blank" href="https://goo.gl/forms/Rudiu48mBe9WjrfI3">aruande vormi</a> kaudu iga nädal. Need aruanded moodustavad 40% aine hindest.</p>
+    <div class="section">
+        <h3>Iseseisev õppetöö</h3>
+        <p>Iga nädal tuleb iseseisvalt lahendada ette antud ülesanded ja/või arendada oma kodust projekti.<br />
+            Tulemused on vaja saata <a target="_blank" href="https://goo.gl/forms/Rudiu48mBe9WjrfI3">aruande vormi</a> kaudu iga nädal. Need aruanded moodustavad 40% aine hindest.</p>
 
-		<table class="striped" style="table-layout:fixed;">
-		<colgroup>
-			<col width="3%">
-			<col width="80%">
-			<col width="17%">
-		</colgroup>
-			<tr>
-				<th>Nr</th>
-				<th>Ülesanne</th>
-				<th>Aruande tähtaeg</th>
-			</tr>
-			<tr>
-				<td>1</td>
-				<td>Tee lõpuni 1. sessiooni ülesanded (vähemalt kuni objektideni leheküljel 10).</td>
-				<td>09.04.2017 23:59</td>
-			</tr>
-			<tr>
-				<td>2</td>
-				<td>Jätka <a href="http://minitorn.tlu.ee/~jaagup/kool/java/loeng/veebijatk/veebijatk.pdf">Jaagup Kippari materjalidega</a> kuni ülesandeni "Mitu kalkulaatorit lehel", lehekülgedel 10 kuni 12.</td>
-				<td>16.04.2017 23:59</td>
-			</tr>
-			<tr>
-				<td>3</td>
-				<td>Alusta koduse projektiga: mõtle välja, mida tegema hakkad; tee valmis rakenduse kõige lihtsamad osad.</td>
-				<td>23.04.2017 23:59</td>
-			</tr>
-			<tr>
-				<td>4</td>
-				<td>...</td>
-				<td>30.04.2017 23:59</td>
-			</tr>
-			<tr>
-				<td>5</td>
-				<td>...</td>
-				<td>07.05.2017 23:59</td>
-			</tr>
-			<tr>
-				<td>6</td>
-				<td>...</td>
-				<td>14.05.2017 23:59</td>
-			</tr>
-			<tr>
-				<td>7</td>
-				<td>...</td>
-				<td>21.05.2017 23:59</td>
-			</tr>
-			<tr>
-				<td>8</td>
-				<td>...</td>
-				<td>28.05.2017 23:59</td>
-			</tr>
-			<tr>
-				<td>9</td>
-				<td>...</td>
-				<td>04.06.2017 23:59</td>
-			</tr>
-			<tr>
-				<td>10</td>
-				<td>...</td>
-				<td>11.06.2017 23:59</td>
-			</tr>
-		</table>
-	</div>
+        <table class="striped" style="table-layout:fixed;">
+            <colgroup>
+                <col width="3%">
+                <col width="80%">
+                <col width="17%">
+            </colgroup>
+            <tr>
+                <th>Nr</th>
+                <th>Ülesanne</th>
+                <th>Aruande tähtaeg</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Tee lõpuni 1. sessiooni ülesanded (vähemalt kuni objektideni leheküljel 10).</td>
+                <td>09.04.2017 23:59</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Jätka <a href="http://minitorn.tlu.ee/~jaagup/kool/java/loeng/veebijatk/veebijatk.pdf">Jaagup Kippari materjalidega</a> kuni ülesandeni "Mitu kalkulaatorit lehel", lehekülgedel 10 kuni 12.</td>
+                <td>16.04.2017 23:59</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Alusta koduse projektiga: mõtle välja, mida tegema hakkad; tee valmis rakenduse kõige lihtsamad osad.</td>
+                <td>23.04.2017 23:59</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>...</td>
+                <td>30.04.2017 23:59</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>...</td>
+                <td>07.05.2017 23:59</td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>...</td>
+                <td>14.05.2017 23:59</td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td>...</td>
+                <td>21.05.2017 23:59</td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td>...</td>
+                <td>28.05.2017 23:59</td>
+            </tr>
+            <tr>
+                <td>9</td>
+                <td>...</td>
+                <td>04.06.2017 23:59</td>
+            </tr>
+            <tr>
+                <td>10</td>
+                <td>...</td>
+                <td>11.06.2017 23:59</td>
+            </tr>
+        </table>
+    </div>
 
-	<div class="divider"></div>
+    <div class="divider"></div>
 
-	<div class="section">
-		<h3>Kodune projekt</h3>
-		<p>Iga õpilane koostab iseseisvalt kodus ühe suurema rakenduse.</p>
-		<p>Projekti sisu mõtleb igaüks ise välja: selleks võiks olla endale huvitaval teemal praktiline rakendus või mäng vms.</p>
-		<ul class="collection with-header">
-			<h5>Olulised punktid on:</h5>
-			<li class="collection-item">Kood versioonihalduses juba projekti alguses,</li>
-			<li class="collection-item">Töötav demorakendus avalikult ligipääsetav,</li>
-			<li class="collection-item">Koodide vastamine standarditele (HTML validaator jne).</li>
-			<li class="collection-item">Täpsemad nõudmised tulevad kursuse jooksul.</li>
-		</ul>
-	</div>
+    <div class="section">
+        <h3>Kodune projekt</h3>
+        <p>Iga õpilane koostab iseseisvalt kodus ühe suurema rakenduse.</p>
+        <p>Projekti sisu mõtleb igaüks ise välja: selleks võiks olla endale huvitaval teemal praktiline rakendus või mäng vms.</p>
+        <ul class="collection with-header">
+            <h5>Olulised punktid on:</h5>
+            <li class="collection-item">Kood versioonihalduses juba projekti alguses,</li>
+            <li class="collection-item">Töötav demorakendus avalikult ligipääsetav,</li>
+            <li class="collection-item">Koodide vastamine standarditele (HTML validaator jne).</li>
+            <li class="collection-item">Täpsemad nõudmised tulevad kursuse jooksul.</li>
+        </ul>
+    </div>
+    <div class="divider"></div>
+    <div class="section">
+        <h3>Hinneteleht</h3>
+        <a href="https://docs.google.com/spreadsheets/d/1qqHICT7NxG3EAAfX8XBeRQgDtp7vDHuxpE6ZyeM8lEA/edit#gid=0">Hinneteleht on saadaval siin.</a>
+    </div>
 
-	<div class="divider"></div>
-	<div class="section">
-		<h3>Eksam</h3>
-		<p>Eksamil on vaja kaitsta oma kodust projekti.</p>
-		<p>Kaitsmise protseduur: õpilane demob oma rakenduse tööd ja koodi klassi ees; vastab küsimustele ning vajadusel muudab/täiendab jooksvalt oma programmi.</p>
-	</div>
+    <div class="divider"></div>
+    <div class="section">
+        <h3>Eksam</h3>
+        <p>Eksamil on vaja kaitsta oma kodust projekti.</p>
+        <p>Kaitsmise protseduur: õpilane demob oma rakenduse tööd ja koodi klassi ees; vastab küsimustele ning vajadusel muudab/täiendab jooksvalt oma programmi.</p>
+    </div>
 
-	<div class="divider"></div>
+    <div class="divider"></div>
 
-	<div class="section">
-		<blockquote>
-			<h4>Jututuba</h4>
-			<p>
-				Kui tegeled aine teemadega, siis ilmuta end ka <a target="_blank" href="https://gitter.im/hki5002/">jututoas</a>!<br/>
-				Saab kaastudengitega suhelda ja abi küsida. Kui väga läbuks ei lähe, siis jälgib seda ka õppejõud ning
-				püüab
-				abistada.<br/>
-				Kui keegi tudeng on jututoas aktiivne abistaja, siis saab selle eest ka lisapunkte!
-			</p>
-		</blockquote>
-		<blockquote>
-			<h4>See lehekülg</h4>
-			<p>
-				Kui leiad trükivea või tahad seda lehte niisama sisulisemaks/ilusamaks/huvitavamaks teha, siis palun väga:
-				<a target="_blank" href="https://github.com/mangus/hki5002">tee pull request GitHub-is</a>.<br />
-				Asjalike täienduste eest lisapunktid!
-			</p>
-		</blockquote>
-	</div>
-	</div>
-	<footer class="page-footer">
-		<div class="container">
-			<div class="row">
-				<div class="col l6 s12">
-					<h5 class="white-text">Programmeerimine I HKI5002</h5>
-					<p class="grey-text text-lighten-4">HKI5002 / 2017 kevadsemester</p>
-				</div>
-				<div class="col l4 offset-l2 s12">
-					<h5 class="white-text">Lingid</h5>
-					<ul>
-						<li><a class="grey-text text-lighten-3" target="_blank" href="https://github.com/mangus/hki5002">Git</a></li>
-						<li><a class="grey-text text-lighten-3" target="_blank" href="https://gitter.im/hki5002/">Jututuba</a></li>
-						<li><a class="grey-text text-lighten-3" target="_blank" href="https://goo.gl/forms/Rudiu48mBe9WjrfI3">Iganädalane aruanne</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<div class="footer-copyright">
-			<div class="container"></div>
-		</div>
-	</footer>
+    <div class="section">
+        <blockquote>
+            <h4>Jututuba</h4>
+            <p>
+                Kui tegeled aine teemadega, siis ilmuta end ka <a target="_blank" href="https://gitter.im/hki5002/">jututoas</a>!<br/>
+                Saab kaastudengitega suhelda ja abi küsida. Kui väga läbuks ei lähe, siis jälgib seda ka õppejõud ning
+                püüab
+                abistada.<br/>
+                Kui keegi tudeng on jututoas aktiivne abistaja, siis saab selle eest ka lisapunkte!
+            </p>
+        </blockquote>
+        <blockquote>
+            <h4>See lehekülg</h4>
+            <p>
+                Kui leiad trükivea või tahad seda lehte niisama sisulisemaks/ilusamaks/huvitavamaks teha, siis palun väga:
+                <a target="_blank" href="https://github.com/mangus/hki5002">tee pull request GitHub-is</a>.<br />
+                Asjalike täienduste eest lisapunktid!
+            </p>
+        </blockquote>
+    </div>
+</div>
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Programmeerimine I HKI5002</h5>
+                <p class="grey-text text-lighten-4">HKI5002 / 2017 kevadsemester</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Lingid</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" target="_blank" href="https://github.com/mangus/hki5002">Git</a></li>
+                    <li><a class="grey-text text-lighten-3" target="_blank" href="https://gitter.im/hki5002/">Jututuba</a></li>
+                    <li><a class="grey-text text-lighten-3" target="_blank" href="https://goo.gl/forms/Rudiu48mBe9WjrfI3">Iganädalane aruanne</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container"></div>
+    </div>
+</footer>
 </body>
 </html>
