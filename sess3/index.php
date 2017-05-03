b19addf2cac0cf39e22e0b5c95c2fef3910c9bd3
Mart Mangus <mart@mangus.ee>
2017-05-03 15:15:15 +0300

    Lisatud 3. praktikumi materjalid

---
+++ sess3/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8; width=device-width, initial-scale=1.0" name="viewport">
+    <title>HKI5002: 3. sessioon</title>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/styles/materialize.min.css">
+    <link rel="stylesheet" href="../assets/styles/main.css">
+</head>
+<body>
+    <div class="page-wrap container">
+        <h1 class="main-heading">HKI5002 kolmas sessioon</h1>
+        <div class="divider"></div>
+        <div class="section">
+            <h3>Tänane tegevusplaan</h3>
+            <ul>
+                <li>Igaüks kaitseb klassi ees oma kodus tehtud töid.</li>
+                <li>Jätkame Jaagup Kippari materjalidega ja proovime sealset koodi paremini vormistada.</li>
+                <li>Töötame kahekesi paaris: teeme paarisprogrammeerimist.</li>
+            </ul>
+        </div>
+
+        <div class="section">
+            <h3>Koduste tööde ülevaaltus</h3>
+            <ul>
+                <li>Tule näita oma kodutööd ja räägi kuidas ja mida seal tegid.</li>
+                <li>Koduse töö kaitsmine moodustab 50% nende eest saadavatest punktidest.</li>
+            </ul>
+        </div>
+
+        <div class="section">
+            <h3>Ülesanded</h3>
+            Jätkame <a href="http://minitorn.tlu.ee/~jaagup/kool/java/loeng/veebijatk/veebijatk.pdf">Jaagup Kippari materjalidega</a>:
+            <ol>
+                <li>lõpeta ära ja pane käima <a href="../sess2">eelmise praktikumi ülesanded</a> -- kui vaja küsi abi klassikaaslastelt ja õppejõult;</li>
+                <li>kasuta kogu praktikumi vältel versioonihaldust;</li>
+                <li>uuri Jaagupi materjalidest "Objekti graafilist väljundit" ja sealseid ülesandeid;</li>
+                <li>püüa eelnev ülesanne paremini vormistada kasutades klasside defineerimiseks <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes">sellist stiili</a>;</li>
+                <li>jätka ülesandega "Foor";</li>
+                <li>kasuta eelneva ülesande klasside defineerimiseks sellist kirjeldust, millest kindlasti aru saad;</li>
+                <li>pane käima ka "Mitu foori";</li>
+                <li>vormista eelmine ülesanne nii, et saad aru, mida iga koodirida teeb;</li>
+                <li>kui jääb aega üle, siis aita kaastudengeid järele.</li>
+            </ol>
+        </div>
+    </div>
+</body>
+</html>
+
