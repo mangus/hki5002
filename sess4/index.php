d836635424fd3643332e7ec6eb3a5f16da32d3c1
Mart Mangus <mart@mangus.ee>
2017-05-18 00:34:28 +0300

    4. praktikum lisatud

---
+++ sess4/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8; width=device-width, initial-scale=1.0" name="viewport">
+    <title>HKI5002: 4. sessioon</title>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/styles/materialize.min.css">
+    <link rel="stylesheet" href="../assets/styles/main.css">
+</head>
+<body>
+    <div class="page-wrap container">
+        <h1 class="main-heading">HKI5002 neljas sessioon</h1>
+        <div class="divider"></div>
+        <div class="section">
+            <h3>Tänane tegevusplaan</h3>
+            <ul>
+                <li>Kaitseme kodus tehtud töid.</li>
+                <li>Paneme Javascript-i serveriga suhtlema.</li>
+                <li>Uurime ja katsetame, mis on XMLHttpRequest ja Websocket.</li>
+                <li>Jätkame paarisprogrammeerimisega.</li>
+            </ul>
+        </div>
+
+        <div class="section">
+            <h3>Ülesanded</h3>
+            <ol>
+                <li>Lõpeta ära ja pane käima <a href="../sess3">eelmise praktikumi ülesanded</a> -- kui vaja küsi abi klassikaaslastelt ja õppejõult.</li>
+                <li>Loe <a href="http://minitorn.tlu.ee/~jaagup/kool/java/loeng/veebijatk/veebijatk.pdf">Jaagup Kippari materjalidest</a> XMLHttpRequest-i kohta tee ära ülesanded leheküljel 74.</li>
+                <li>Liigu Jaagupi materjalidega edasi ja lahenda ka ülesanded leheküljel 78.</li>
+                <li>Uuri, mis asi on Websocket ja tee läbi <a href="http://www.binarytides.com/websockets-php-tutorial/">see tutorial</a>.</li>
+                <li>kui jääb aega üle, siis aita kaastudengeid järele.</li>
+            </ol>
+        </div>
+    </div>
+</body>
+</html>
+
