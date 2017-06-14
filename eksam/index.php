509b2d3889d5cff5f5478825fb724994b5f009d9
Mart Mangus <mart@mangus.ee>
2017-06-14 21:57:11 +0300

    Eksami kirjeldus lisatud

---
+++ eksam/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8; width=device-width, initial-scale=1.0" name="viewport">
+    <title>HKI5002: Eksam</title>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/styles/materialize.min.css">
+    <link rel="stylesheet" href="../assets/styles/main.css">
+</head>
+<body>
+    <div class="page-wrap container">
+        <h1 class="main-heading">HKI5002 eksam</h1>
+        <div class="divider"></div>
+        <div class="section">
+		<p>
+		    Täna vaatame üle teie kursuse jooksul tehtud projektid ja paneme nende eest hinded.<br />
+		    Kaitsmisel on oluline roll ka kaastudengitel, kes saavad kaasa rääkida ja hinnet mõjutada.<br />
+		    Abivahendina kasutame seekord <a href="https://tlk.io/hki5002">seda jututuba</a>.
+		</p>
+        </div>
+        <div class="section">
+            <h3>Eksmi protseduur:</h3>
+            <ol>
+		<li>õpilane postitab jututuppa lingi oma programmi koodile versioonihalduses;</li>
+    		<li>õpilane demob klassi eest (kas õppejõu arvutist või oma arvutist) oma programmi -- et mis see teeb ja miks hea on (1-2 minutit);</li>
+    		<li>õpilane näitab programmi koodi ja selgitab keerulisemaid kohti (3-5 minutit);</li>
+		<li>õpilane vastab teiste õpilaste ja õppejõu küsimustele ning vajadusel teeb koodis väiksemaid muudatusi (2-4 minutit);</li>
+		<li>klassis olevad õpilased pakuvad kaitsnud õpilasele jututoa kaudu hinnet;</li>
+		<li>õppejõud määrab hinde.</li>
+            </ol>
+        </div>
+        <div class="section">
+            <h3>Tagasiside</h3>
+	    <p>Ehk leiad eksami lõpus või pärast tundi mõne minuti, et täita ära <a href="https://goo.gl/forms/HypXFyLJPmsOdI3L2">lihtne tagasiside küsimustik</a>. Aitäh!</p>
+        </div>
+    </div>
+</body>
+</html>
+
